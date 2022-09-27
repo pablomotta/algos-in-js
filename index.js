dcdb5ea1b1e2d9d4a58f55d73d839dbea6de5963
Pablo Motta <pmotta.dev@gmail.com>
2022-09-27 11:23:06 -0700

    First recurring character

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const numbers = [1, 3, 5, 4, 5, 7, 8, 9];
-
+const numbers = [0, 1, 3, 5, 4, 0, 7, 8, 9];
+// Time O(n) - Space O(n)
 function firstRecurringChar(array) {
 	const hashMap = new Map();
 	for (let i = 0; i < array.length; i++) {
@@ -13,3 +13,17 @@ function firstRecurringChar(array) {
 
 // returns 5
 console.log(firstRecurringChar(numbers));
+
+// Time O(n) - Space O(n)
+function firstRecurringChar2(array) {
+	const hashMap = {};
+	for (let i = 0; i < array.length; i++) {
+		if (hashMap[array[i]]) {
+			return array[i];
+		}
+		hashMap[array[i]] = true;
+	}
+	return undefined;
+}
+
+console.log(firstRecurringChar2(numbers));
