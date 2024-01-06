6c8d0bf109ce6bc4784565bb830f4797f894cff9
Pablo Motta <pmotta.dev@gmail.com>
2024-01-06 01:07:12 +0000

    number of steps v1

--- integers/numberOfSteps.js
+++ integers/numberOfSteps.js
@@ -1,5 +1,15 @@
 const numberOfSteps = (num) => {
-    return num
+    let steps = 0;
+    while (num > 0) {
+      if (num % 2 === 1) {
+        num = num - 1;
+        steps = steps + 1;
+      } else if (num % 2 === 0) {
+        num = num / 2;
+        steps = steps + 1;
+      }
+    }
+    return steps;   
 }
 
 console.log(numberOfSteps(14))
