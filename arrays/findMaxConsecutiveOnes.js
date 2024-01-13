ee759b830a1445a9e4e064e9d3e1dac3e8eb535e
Pablo Motta <pmotta.dev@gmail.com>
2024-01-13 00:37:11 +0000

    WIP findMaxConsecutiveOnes

--- arrays/findMaxConsecutiveOnes.js
+++ arrays/findMaxConsecutiveOnes.js
@@ -3,6 +3,28 @@
  * @return {number}
  */
 var findMaxConsecutiveOnes = function(nums) {
-    console.log(num)
-    
+    let count = 0;
+    let maxCount = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1] && i !== 0) {
+            console.log({
+                curr: nums[i],
+                prev: nums[i - 1]
+            })
+            if (count > maxCount) maxCount = count 
+            count = 0
+        }
+        count++
+    }
+    return maxCount   
 };
+
+// const numsArr = [1,0,1,1,0,1]
+// console.log('TEST 1:')
+// console.log(findMaxConsecutiveOnes(numsArr))
+// console.log('TEST 2:')
+// const numsArr2 = [1,1,1,0,0,1,1,0,0,0,0,1]
+// console.log(findMaxConsecutiveOnes(numsArr2))
+console.log('TEST 3:')
+const numsArr3 = [1,1,0,1]
+console.log(findMaxConsecutiveOnes(numsArr3))
