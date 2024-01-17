1624f141dae6f23972c4f29627cbd259e29a26fb
Pablo Motta <pmotta.dev@gmail.com>
2024-01-17 06:06:26 +0000

    sortedSquares version 1

--- arrays/sortedSquares.js
+++ arrays/sortedSquares.js
@@ -1,7 +1,15 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
+ * https://leetcode.com/problems/squares-of-a-sorted-array/description/
  */
 var sortedSquares = function(nums) {
-    
+    let squaredArr = []
+    for (let num of nums) {
+        const squaredNum = Math.pow(num, 2)
+        squaredArr.push(squaredNum)
+    }
+    return squaredArr.sort((a, b) => a - b)
 };
+
+console.log(sortedSquares([-7,-3,2,3,11]))
