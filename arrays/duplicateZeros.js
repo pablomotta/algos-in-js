c612ec6a7440a1d6083dfc0f278bb0c239ab6916
Pablo Motta <pmotta.dev@gmail.com>
2024-01-20 07:06:34 +0000

    duplicateZeros final

--- arrays/duplicateZeros.js
+++ arrays/duplicateZeros.js
@@ -4,7 +4,15 @@
  * https://leetcode.com/problems/duplicate-zeros/description/
  */
 const duplicateZeros = function(arr) {
+    const length = new Array(arr.length)
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === 0) {
+            // Insert a new 0 right after the current 0
+            arr.splice(i, 0, 0);
+            arr.pop()
+        }
+    }
     return arr
 };
 
-console.log(duplicateZeros([1,3,2,0,4,4,3,5]))
+console.log(duplicateZeros([1,0,2,3,0,4,5,0]))
