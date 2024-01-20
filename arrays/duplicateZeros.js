b4aba03eb1e510c1400f16e81c1c49b7e041393f
Pablo Motta <pmotta.dev@gmail.com>
2024-01-20 05:54:47 +0000

    Add test case

--- arrays/duplicateZeros.js
+++ arrays/duplicateZeros.js
@@ -1,7 +1,10 @@
 /**
  * @param {number[]} arr
  * @return {void} Do not return anything, modify arr in-place instead.
+ * https://leetcode.com/problems/duplicate-zeros/description/
  */
 const duplicateZeros = function(arr) {
-    
+    return arr
 };
+
+console.log(duplicateZeros([1,3,2,0,4,4,3,5]))
