8ff73fa6a480556ebbdf60d6bcb910e7877d75ba
Pablo Motta <pmotta.dev@gmail.com>
2024-02-01 03:01:59 +0000

    V1 final solution

--- arrays/mergeSortedArray.js
+++ arrays/mergeSortedArray.js
@@ -6,7 +6,10 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    return {nums1, m, nums2, n}
+    nums1.length = m;
+    nums2.length = n;
+    nums1.push(...nums2);
+    nums1.sort((a,b) => a - b)
 };
 
 const nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
