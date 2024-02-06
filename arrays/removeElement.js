3d81ac5a9ede8592c53fda39c8c01a83e6787346
Pablo Motta <pmotta.dev@gmail.com>
2024-02-06 17:18:17 +0000

    removeElement solution

--- arrays/removeElement.js
+++ arrays/removeElement.js
@@ -4,9 +4,17 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    return {nums, val}
+    let writer = 0
+    for (let reader = 0; reader < nums.length; reader++) {
+        if (nums[reader] !== val) {
+            nums[writer] = nums[reader]
+            writer++ 
+        }
+    }
+    return writer
 };
 
 // Tests
-const nums = [0,1,2,2,3,0,4,2], val = 2
+const nums = [0,1,2,2,3,0,4,2]
+const val = 2
 console.log(removeElement(nums, val))
