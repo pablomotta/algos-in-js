78c0bde57cc799a28c9cf25d03164aca68cd2f9c
Pablo Motta <pmotta.dev@gmail.com>
2024-03-14 14:13:28 -0700

    Add checkIfExists solution

--- arrays/checkIfExists.js
+++ arrays/checkIfExists.js
@@ -2,4 +2,19 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var checkIfExist = function (arr) {};
+const checkIfExist = function(arr) {
+    const checkSet = new Set()
+    for (let i = 0; i < arr.length; i++) {
+        if (checkSet.has(arr[i] * 2 )) {
+            return true 
+        }
+        if (arr[i] % 2 === 0 && checkSet.has(arr[i] / 2)) {
+            return true
+        }
+        checkSet.add(arr[i])
+    }
+    return false   
+};
+
+const arr = [10, 2, 5, 3];
+console.log(checkIfExist(arr)); 
