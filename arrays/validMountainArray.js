93b43f00fadb7c62d99fac871bfeb9d505351826
Pablo Motta <pmotta.dev@gmail.com>
2024-03-14 20:38:54 -0700

    Add second solution

--- arrays/validMountainArray.js
+++ arrays/validMountainArray.js
@@ -23,3 +23,14 @@ var validMountainArray = function (arr) {
 	// If we've successfully walked the whole array, i will be at the last index
 	return i === arr.length - 1;
 };
+
+var validMountainArray2 = function (arr) {
+	let i = 0;
+	let j = arr.length - 1;
+
+	while (arr[i] < arr[i + 1]) i++;
+	while (arr[j - 1] > arr[j]) j--;
+	if (i == arr.length - 1) return false;
+	if (j == 0) return false;
+	return i == j ? true : false;
+};
