bc83891d7eb1142e36694d82a64a28e9dd719d3c
Pablo Motta <pmotta.dev@gmail.com>
2024-03-14 20:36:10 -0700

    Add solution one for validMountainArray

--- arrays/validMountainArray.js
+++ arrays/validMountainArray.js
@@ -2,4 +2,24 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var validMountainArray = function (arr) {};
+var validMountainArray = function (arr) {
+	if (arr.length < 3) return false;
+
+	let i = 0;
+
+	// Ascend to the peak
+	while (i < arr.length - 1 && arr[i] < arr[i + 1]) {
+		i++;
+	}
+
+	// Peak can't be the first or last
+	if (i === 0 || i === arr.length - 1) return false;
+
+	// Descend from the peak
+	while (i < arr.length - 1 && arr[i] > arr[i + 1]) {
+		i++;
+	}
+
+	// If we've successfully walked the whole array, i will be at the last index
+	return i === arr.length - 1;
+};
